df772ab4dd5d67c39f7bf177c3b5815a10af221d
Mateus Barcelos <mateuspit@hotmail.com>
2023-03-17 12:41:33 -0300

    HTML and CSS done, without logic. Still need to do loading gif and progress bar

--- src/pages/HistoryPage/HistoryPage.jsx
+++ src/pages/HistoryPage/HistoryPage.jsx
@@ -3,6 +3,25 @@ import FooterHabits from "../../components/FooterHabits";
 import HeaderHomeUser from "../../components/HeaderHomeUser";
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css'
+import { formatDate } from "react-calendar/dist/esm/shared/dateFormatter";
+
+function paintSomeDays(date, view){
+    if((date.getDate() === 27 && date.getMonth() === 1) || 
+    (date.getDate() === 13 && date.getMonth() === 2) || 
+    (date.getDate() === 1 && date.getMonth() === 2) || 
+    (date.getDate() === 8 && date.getMonth() === 2)
+    || (date.getDate() === 10 && date.getMonth() === 2)
+    || (date.getDate() === 17 && date.getMonth() === 2)
+    || (date.getDate() === 3 && date.getMonth() === 2)){
+        return "days-did";
+    }
+    else if (
+        (date.getDate() === 6 && date.getMonth() === 2)|| 
+        (date.getDate() === 20 && date.getMonth() === 2)|| 
+        (date.getDate() === 15 && date.getMonth() === 2)){
+        return "day-not-did"
+    }
+}
 
 export default function HistoryPage() {
     return (
@@ -10,12 +29,56 @@ export default function HistoryPage() {
             <HeaderHomeUser />
             <ContainerIphone8>
                 <PageHistoryTitle>Histórico</PageHistoryTitle>
-                <Calendar calendarType="US" locale="pt"/>
+                {/* <StyledCalendar formatDay={(locale, date) => formatDate(date, 'd')} calendarType="US" locale="pt"/> */}
+                <StyledCalendar 
+                tileClassName={({ activeStartDate, date, view }) => paintSomeDays(date,view)}
+                calendarType="US" 
+                locale="pt"/>
             </ContainerIphone8>
             <FooterHabits />
         </>
     );
 }
+
+const StyledCalendar = styled(Calendar)`
+    &&{
+        border: none;
+        font-family: 'Lexend Deca';
+        font-style: normal;
+        font-weight: 400;
+        width: 335px;
+        height: 402px;
+        margin-left: 19px;
+    }
+    .react-calendar__tile--active {
+        background: #006edc;
+        color: white;
+        /* background-clip: padding-box; */
+        border-radius: 50%;
+        abbr{
+            transform: scale(1.25);
+            font-size: 15px;
+        }
+    }
+    .react-calendar__month-view__days__day{
+        /* color: aqua; */
+        margin-bottom: 5px;
+        margin-top: 10px;
+        font-size: 12px;
+        font-family: 'Lexend Deca';
+        font-style: normal;
+        font-weight: 400;
+    }
+    .days-did{
+        /* color: aqua; */
+        background-color: #8cc654;
+        border-radius: 50%;
+    }
+    .day-not-did{
+        background-color: #ea5766;
+        border-radius: 50%;
+    }
+`;
 
 const ContainerIphone8 = styled.div`
     width: 375px;
